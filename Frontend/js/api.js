92a28cefff40cc06581fc162249b29631fb98704
Juan Hoyos <juanandreshoyosocampo@gmail.com>
2026-05-31 20:11:11 -0500

    ...

--- Frontend/js/api.js
+++ Frontend/js/api.js
@@ -2,7 +2,7 @@
 // api.js - Comunicación con el Backend
 // ==========================================
 
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = 'saferoutemedellin-production.up.railway.app';
 
 const SafeRouteAPI = {
     async calculateRoute(origen, destino, alpha, beta, mode = "both") {
